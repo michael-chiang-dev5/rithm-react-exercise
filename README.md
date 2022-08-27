38ebdbcd303632f878b625f23c2caa9c16946870
Michael Chiang <michael.chiang.mc5@gmail.com>
2022-08-27 14:59:39 -0700

    added list

--- README.md
+++ README.md
@@ -2,8 +2,9 @@
 Implements a solution to https://github.com/rithmschool/react_exercises/tree/master/01-introduction-to-react/jsx-and-babel
 
 ## Notes
-React componenet return needs to be wrapped in a div
+### React componenet return needs to be wrapped in a div
 ```javascript
+/* Note the presence of a div */
 class App extends React.Component {
     render() {
         return (
@@ -13,10 +14,21 @@ class App extends React.Component {
 ```
 
 ```javascript
+/* Note the absence of a div */
 class App extends React.Component {
     render() {
         return (
             <p>This doesn't work</p>
         );
 }}
+```
+
+### The render return value is something like an array
+```javascript
+/* arr should be something like [<li>item1</li>, <li>item2</li>] */
+/* you can also use map instead to generate arr, for example [item1,item2].map( (e)=><li>{e}</li> ) */
+    render() {
+        return (
+        <div>{arr}</div>
+        )}
 ```
--- appPart3.jsx
+++ appPart3.jsx
@@ -1,16 +1,92 @@
 class Person extends React.Component {
     render() {
         let age = this.props.age;
+        let name = this.props.name;
+        if (name.length>4) name=name.slice(0,4)
+        let hobbies = this.props.hobbies
         return (
             <div>
             <p>Learn some more information about this person</p>
+            {name}
             {age >= 21 ? (<h3>have a drink</h3>) : (<h3>you must be 21 to drink</h3>)}
+            <ul>
+                {hobbies.map((e)=><li>{e}</li>)}
+            </ul>
             </div>
         );
     }
 }
 
+class Name extends React.Component {
+    render() {
+        let name = this.props.name;
+        if (name.length>4) name=name.slice(0,4)
+        return (
+            <div>
+                {name}
+            </div>
+        );
+    }
+}
+
+class Age extends React.Component {
+    render() {
+        let age = this.props.age;
+        return (
+            <div>
+                {age >= 21 ? (<h3>have a drink</h3>) : (<h3>you must be 21 to drink</h3>)}
+            </div>
+        )
+    }
+}
+
+class Hobbies1 extends React.Component {
+    render() {
+        let hobbies = this.props.hobbies;
+        const rn = []
+        for (let hobby of hobbies) {
+            rn.push(<li>{hobby}</li>);
+        }   
+        return (
+            <div>
+                <h3>Using array</h3>
+                {rn}
+            </div>
+        )
+    }
+}
+
+class Hobbies2 extends React.Component {
+    render() {
+        let hobbies = this.props.hobbies;        
+
+        return (
+            <div>
+                <h3>Using map</h3>
+                {hobbies.map((e)=><li>{e}</li>)}
+            </div>
+        )
+    }
+}
+
+class App extends React.Component {
+    render() {
+        let name = this.props.name;
+        let age = this.props.age;
+        let hobbies = this.props.hobbies
+
+        return (
+            <div>
+                <p>Learn some more information about this person</p>
+                <Name name={name}/>
+                <Age age={age}/>
+                <Hobbies1 hobbies={hobbies} />
+                <Hobbies2 hobbies={hobbies} />
+            </div>                
+        );
+    }
+}
 
 
 //ReactDOM.render(<div>test</div>, document.getElementById('person'))
-ReactDOM.render(<Person age='20'/>, document.getElementById("person"));
+ReactDOM.render(<App age='20' name='michael' hobbies={['tennis','running','reading']}/>, document.getElementById("person"));
